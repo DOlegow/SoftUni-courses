2b4cd8d308bd4748af2eac01d0871ae3189ea494
Данаил Данаилов <134785851+DOlegow@users.noreply.github.com>
2024-06-06 16:29:25 +0300

    phonebook
    learn django

--- fundamentals_python_homework_tasks/Basic_WEB_project/djangoproject/djangoproject/phonebook/views.py
+++ fundamentals_python_homework_tasks/Basic_WEB_project/djangoproject/djangoproject/phonebook/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render, redirect
+from djangoproject.phonebook.models import Contact
 
 def landing_page(request):
-    return render(request, "phonebook/index.html")
+    context = {
+        'contacts': Contact.objects.all(),
+    }
+    return render(request, 'phonebook/index.html', context)
+
+def create_contact(request):
+    name = request.POST['name']
+    number = request.POST['number']
+    contact = Contact(
+        name=name,
+        number=number
+    )
+    contact.save()
+    return redirect('landing-page')
